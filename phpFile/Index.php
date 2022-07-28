7137b50ac4429b48e8755611d0afa86d863aea7d
Hermogenes II Pelaez-Magsino <hermogenes.magsino2@gmail.com>
2022-07-28 21:42:05 +0800

    first commit - committing all files to the reposotiry

---
+++ phpFile/Index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="../cssFiles/Index.css">
+
+  <title>Servi-Seek</title>
+
+</head>
+<body>
+<div id="page_container">
+      <header id="header" role="banner">
+        <section class="header_left_section">
+          <input
+            type="text"
+            class="search_area"
+            placeholder="Search Services"
+          />
+          <button class="search_button">
+            <img src="../images/search.svg" alt="Search Button" />
+          </button>
+        </section>
+        <div class="header_right_section">
+          <div class="right-navigation"><p>All Categories</p></div>
+          <div class="right-navigation"><p>Map</p></div>
+          <div class="right-navigation"><p>Login/Register</p></div>
+          <div class="profile">
+            <img src="../images/user-solid.svg" alt="Profile Picture" />
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <!--FrontPage-->
+        <section id="front-page" class="main-section">
+          <div class="title-container">
+            <h1>Servi-Seek</h1>
+            <h2>Marinduque's Marketplace of Services</h2>
+          </div>
+        </section>
+
+        <!-- Categories -->
+        <section id="categories-page" class="main-section">
+          <h1 class="categories-title">Categories</h1>
+          <div class="categories-grid">
+            <div class="categories">
+              <div class="categories-name">Utility</div>
+              <ul class="sub-categories">
+                <li>Electrician</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Electronic Devices</div>
+              <ul class="sub-categories">
+                <li>Smartphones</li>
+                <li>Laptop</li>
+                <li>Personal Computer</li>
+                <li>Smart Television</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Mechanic</div>
+              <ul class="sub-categories">
+                <li>Vulcanize</li>
+                <li>Vehicle Repair</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Garments</div>
+              <ul class="sub-categories">
+                <li>Tailor</li>
+                <li>Renting</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Furniture</div>
+              <ul class="sub-categories"></ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Digital Printing</div>
+              <ul class="sub-categories">
+                <li>Photocopy</li>
+                <li>Book Bind</li>
+                <li>Photoshoot</li>
+                <li>Garments Printing</li>
+                <li>Tarpaulin</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Beauty Services</div>
+              <ul class="sub-categories">
+                <li>Haircut</li>
+                <li>Pedicure/Manicure</li>
+                <li>Make-up</li>
+                <li>Massage/Spa</li>
+              </ul>
+            </div>
+            <div class="categories">
+              <div class="categories-name">Others</div>
+              <ul class="sub-categories"></ul>
+            </div>
+          </div>
+        </section>
+        <section id="service-provider-page" class="main-section">
+          <div class="service-provider-title">
+            <h1>Service Provider</h1>
+            <p>Electronic Device</p>
+          </div>
+          <div class="service-provider-container">
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+            <div class="service-provider">
+              <div class="service-provider-profile-container">
+                <img
+                  class="service-provider-profile"
+                  src="../images/user-solid.svg"
+                  alt=""
+                />
+              </div>
+              <div class="service-provider-info-container">
+                <div class="service-provider-info">
+                  <div class="service-provider-info">
+                    <p class="name">Name: Hermogenes II Pelaez-Magsino</p>
+                    <p class="name">Age:  21</p>
+                    <p class="name">Location: Dolores Santa Cruz Marinduque</p>
+                    <p class="name">Electrician</p>
+                  </div>
+                </div>
+                <button class="view-button">View</button>
+              </div>
+            </div>
+          <div class="service-provider-more"><p>See more</p></div>
+        </section>
+        <section id="other-page" class="main-section"><div></div></section>
+        <section id="other-page" class="main-section"><div></div></section>
+      </main>
+    </div>
+    <script></script>
+</body>
+</html>
